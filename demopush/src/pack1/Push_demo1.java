9bde1d098cad35e316630cad15ad3c6ea10c5f4d
arun_565 <kundaarunkumar2002@gmail.com>
2023-10-30 11:30:30 +0530

    first commit

---
+++ demopush/src/pack1/Push_demo1.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Push_demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This demo tom push files from ecljpse to github");
+	}
+
+}
