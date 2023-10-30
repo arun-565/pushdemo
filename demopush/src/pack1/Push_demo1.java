7718943aa5d8a1fa6fbc7e064d4bac8b93d35c47
arun_565 <kundaarunkumar2002@gmail.com>
2023-10-30 12:04:51 +0530

    3

--- demopush/src/pack1/Push_demo1.java
+++ demopush/src/pack1/Push_demo1.java
@@ -4,6 +4,7 @@ public class Push_demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int s;
 		System.out.println("This demo tom push files from ecljpse to github");
 	}
 
